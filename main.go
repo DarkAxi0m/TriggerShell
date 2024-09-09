29717425d747ce1a5bfe6e850983fbdfa5b276c3
Christopher Chase (DarkAxi0m) <1772922+DarkAxi0m@users.noreply.github.com>
2024-09-09 17:18:34 +0930

    go fmt

--- main.go
+++ main.go
@@ -12,12 +12,10 @@ import (
 	"github.com/fsnotify/fsnotify"
 )
 
-// A map to hold file suffix to script mapping
 var suffixToScriptMap = make(map[string]string)
 var mutex sync.Mutex
 
 func main() {
-	// Target directory and script folder as params
 	if len(os.Args) < 3 {
 		log.Fatalf("Usage: %s <target-directory> <script-directory>\n", os.Args[0])
 	}
@@ -25,20 +23,17 @@ func main() {
 	targetDir := os.Args[1]
 	scriptDir := os.Args[2]
 
-	// Load .sh scripts and map them to file suffixes
 	err := loadScripts(scriptDir)
 	if err != nil {
 		log.Fatalf("Failed to load scripts: %s", err)
 	}
 
-	// Monitor the target directory for new files
 	err = monitorDirectory(targetDir)
 	if err != nil {
 		log.Fatalf("Failed to monitor directory: %s", err)
 	}
 }
 
-// Load .sh scripts from the script directory and create suffix mappings
 func loadScripts(scriptDir string) error {
 	files, err := os.ReadDir(scriptDir)
 	if err != nil {
@@ -58,7 +53,6 @@ func loadScripts(scriptDir string) error {
 	return nil
 }
 
-// Monitor the target directory for new files
 func monitorDirectory(targetDir string) error {
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
@@ -84,17 +78,15 @@ func monitorDirectory(targetDir string) error {
 
 				fileInfo, err := os.Stat(event.Name)
 				if err == nil && !fileInfo.IsDir() && (fileInfo.Size() > 0) {
-                    log.Println("------------------------------")
+					log.Println("------------------------------")
 					log.Printf("> %d  %s\n", fileInfo.Size(), event.Name)
-					//<M-C-S-F5>|| event.Op&fsnotify.Rename == fsnotify.Rename {
-					// When a new file is created, check if the suffix matches any of the scripts
+					//|| event.Op&fsnotify.Rename == fsnotify.Rename {
 					fileExtension := filepath.Ext(event.Name)
 					mutex.Lock()
 					scriptPath, exists := suffixToScriptMap[strings.ToLower(fileExtension)]
 					mutex.Unlock()
 
 					if exists {
-						// Execute the script with the file name as an argument
 						go runScript(scriptPath, event.Name)
 					}
 				}
@@ -109,7 +101,6 @@ func monitorDirectory(targetDir string) error {
 	}
 }
 
-// Run the .sh script passing the new file name as an argument
 func runScript(scriptPath, fileName string) {
 	cmd := exec.Command("bash", scriptPath, fileName)
 	cmd.Stdout = os.Stdout
