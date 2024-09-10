dee6b3d11a827b926207aa534c73073f4d11609a
Christopher Chase <1772922+DarkAxi0m@users.noreply.github.com>
2024-09-10 12:16:40 +0930

    Update appimage.sh

--- example/appimage.sh
+++ example/appimage.sh
@@ -6,31 +6,34 @@ if [ -z "$1" ]; then
 fi
 
 APPIMAGE_FILE="$1"
-APP_NAME=$(basename "$APPIMAGE_FILE" .AppImage)
+ORG_NAME=$(basename "$APPIMAGE_FILE" .AppImage)
 APPLICATIONS_DIR=~/Applications
-DESKTOP_FILE=~/Desktop/$APP_NAME.desktop
+DESKTOP_FILE=~/Desktop/$ORG_NAME.desktop
+DESKTOP_ICON="$(dirname "$(realpath "$0")")/../TriggerShell.png"
 
-zenity --question --text="Do you want to install the AppImage for $APP_NAME?" --width=300
+zenity --question --text="Do you want to install the AppImage for $ORG_NAME?" --width=300
 if [ $? -ne 0 ]; then
     echo "Installation canceled."
     exit 1
 fi
 
-APP_NAME=$(zenity --entry --text="Enter the application name" --entry-text="$APP_NAME" --width=300)
+APP_NAME=$(zenity --entry --text="Enter the application name" --entry-text="$ORG_NAME" --width=300)
 if [ -z "$APP_NAME" ]; then
     echo "No application name provided. Installation canceled."
     exit 1
 fi
 
 mkdir -p "$APPLICATIONS_DIR"
+cp "$DESKTOP_ICON" "$APPLICATIONS_DIR/$ORG_NAME.png"
+
 mv "$APPIMAGE_FILE" "$APPLICATIONS_DIR/"
-chmod +x "$APPLICATIONS_DIR/$APP_NAME.AppImage"
+chmod +x "$APPLICATIONS_DIR/$ORG_NAME.AppImage"
 
 cat <<EOF > "$DESKTOP_FILE"
 [Desktop Entry]
 Name=$APP_NAME
-Exec=$APPLICATIONS_DIR/$APP_NAME.AppImage
-Icon=$APPLICATIONS_DIR/$APP_NAME
+Exec=$APPLICATIONS_DIR/$ORG_NAME.AppImage
+Icon=$APPLICATIONS_DIR/$ORG_NAME.png
 Type=Application
 Terminal=false
 EOF
@@ -39,4 +42,3 @@ chmod +x "$DESKTOP_FILE"
 
 zenity --info --text="AppImage installed successfully!\n\nApp: $APP_NAME\nLocation: $APPLICATIONS_DIR" --width=300
 echo "AppImage moved to $APPLICATIONS_DIR and .desktop file created at $DESKTOP_FILE"
-
